3b7720707ec41a8434897522075722a6fa5000e8
mikeheddes <mikeheddes@gmail.com>
2022-05-02 13:44:05 -0700

    Fix missing datasets package

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ See:
 https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="hdc",
-    version="0.5.5",
+    version="0.5.6",
     description="Python library for Hyperdimensional Computing",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/mikeheddes/hdc-lib",
     license="MIT",
     install_requires=open("requirements.txt").readlines(),
-    packages=["hdc"],
+    packages=find_packages(exclude=['docs', 'hdc.tests', 'examples']),
     python_requires=">=3.6, <4",
     project_urls={
         "Source": "https://github.com/mikeheddes/hdc-lib/",
