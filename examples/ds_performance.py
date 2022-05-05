84ec6e6d3dc669912f03c22639b1dae7ac9f54aa
mikeheddes <mikeheddes@gmail.com>
2022-05-04 20:51:14 -0700

    Add binding based sequences and tuples

--- examples/ds_performance.py
+++ examples/ds_performance.py
@@ -11,31 +11,31 @@ from torchhd import structures, functional
 import string
 
 # Setup
-
 DIMENSIONS = 10000
 itemMemory = structures.Memory()
 letters = list(string.ascii_lowercase)
 letters_hv = functional.random_hv(len(letters), DIMENSIONS)
 list(map(lambda l: itemMemory.add(l[0], l[1]), zip(letters_hv, letters)))
 
-# SETS
 
+# SETS
 exampleSet = ["a", "b", "c", "d", "e"]
 hdSet = structures.Multiset(DIMENSIONS)
 list(map(lambda l: hdSet.add(letters_hv[letters.index(l)]), exampleSet))
 similarity = functional.cosine_similarity(hdSet.value, letters_hv)
 
-# SEQUENCE
 
+# SEQUENCE
+# Bundling based
+exampleSet = ["a", "b", "c", "d", "e"]
 hdSequence = structures.Sequence(DIMENSIONS)
 list(map(lambda l: hdSequence.append(letters_hv[letters.index(l)]), exampleSet))
-# Pop last value
 
 last_value = hdSequence[4]
 similarity = functional.cosine_similarity(last_value, letters_hv)
 
-hdSequence.pop(hdSequence[4])
-hdSequence.append(letters_hv[letters.index("z")])
+# Replacing "e" to "z" at 5th position in  the  sequence
+hdSequence.replace(4, letters_hv[letters.index("e")], letters_hv[letters.index("z")])
 
 exampleSetZ = ["a", "b", "c", "d", "z"]
 hdSequenceZ = structures.Sequence(DIMENSIONS)
@@ -44,6 +44,7 @@ list(map(lambda l: hdSequenceZ.append(letters_hv[letters.index(l)]), exampleSetZ
 similarity = functional.cosine_similarity(
     hdSequenceZ.value, hdSequence.value.unsqueeze(0)
 )
+
 
 # SHIFT AND CONCATENATE
 hdSequence = structures.Sequence(DIMENSIONS)
@@ -57,24 +58,37 @@ hdSequence.concat(hdSequenceConcat)
 
 similarity = functional.cosine_similarity(letters_hv, hdSequence.value)
 
+
 # BINDING BASED REPRESENTATION
 exSeq = ["a", "b", "c", "d", "e"]  # sequence to represent
-hdSequenceOne = structures.Sequence(DIMENSIONS)
+hdSequenceOne = structures.DistinctSequence(DIMENSIONS)
 list(map(lambda l: hdSequenceOne.append(letters_hv[letters.index(l)]), exSeq))
 
 exSeqTwo = ["a", "b", "c", "d", "d"]  # sequence to represent
-hdSequenceTwo = structures.Sequence(DIMENSIONS)
+hdSequenceTwo = structures.DistinctSequence(DIMENSIONS)
 list(map(lambda l: hdSequenceTwo.append(letters_hv[letters.index(l)]), exSeqTwo))
 
 similarity = functional.cosine_similarity(
     hdSequenceOne.value, hdSequenceTwo.value.unsqueeze(0)
 )
 
-"""
+# Replacing "e" to "z" at 5th position in  the  sequence
+hdSequenceOne.replace(4, letters_hv[letters.index("e")], letters_hv[letters.index("z")])
+
+exSeqScratch = ["a", "b", "c", "d", "z"]  # sequence to represent
+hdSequenceScratch = structures.DistinctSequence(DIMENSIONS)
+list(
+    map(lambda l: hdSequenceScratch.append(letters_hv[letters.index(l)]), exSeqScratch)
+)
+
+similarity = functional.cosine_similarity(
+    hdSequenceOne.value, hdSequenceScratch.value.unsqueeze(0)
+)
+
 
 # TUPLES
-exSetOne = ['a', 'b', 'c', 'd', 'e']  # sequence to represent
-exSetTwo = ['x', 'y', 'z']  # sequence to represent
+exSetOne = ["a", "b", "c", "d", "e"]  # sequence to represent
+exSetTwo = ["x", "y", "z"]  # sequence to represent
 
 hdTensorsOne = torch.stack(list(map(lambda l: letters_hv[letters.index(l)], exSetOne)))
 hdTensorsTwo = torch.stack(list(map(lambda l: letters_hv[letters.index(l)], exSetTwo)))
@@ -84,21 +98,19 @@ hdSetTwo = functional.multiset(hdTensorsTwo)
 
 hdTuples = functional.bind(hdSetOne, functional.permute(hdSetTwo, shifts=1))
 
-axSequence = structures.Sequence(DIMENSIONS)
-axSequence.append(letters_hv[letters.index('a')])
-axSequence.append(letters_hv[letters.index('x')])
+axSequence = structures.DistinctSequence(DIMENSIONS)
+axSequence.append(letters_hv[letters.index("a")])
+axSequence.append(letters_hv[letters.index("x")])
 
 similarity = functional.cosine_similarity(axSequence.value, hdTuples.unsqueeze(0))
 
-asSequence = structures.Sequence(DIMENSIONS)
-asSequence.append(letters_hv[letters.index('a')])
-asSequence.append(letters_hv[letters.index('s')])
+asSequence = structures.DistinctSequence(DIMENSIONS)
+asSequence.append(letters_hv[letters.index("a")])
+asSequence.append(letters_hv[letters.index("s")])
 similarity = functional.cosine_similarity(asSequence.value, hdTuples.unsqueeze(0))
 
-"""
 
 # UNDIRECTED GRAPH
-
 edges = [("a", "b"), ("a", "e"), ("c", "b"), ("d", "c"), ("e", "d")]
 graph = structures.Graph(DIMENSIONS, directed=False)
 list(
@@ -110,7 +122,7 @@ list(
     )
 )
 
-Aneighbours = graph.node_neighbours(letters_hv[letters.index("a")])
+Aneighbours = graph.node_neighbors(letters_hv[letters.index("a")])
 
 similarity = functional.cosine_similarity(Aneighbours, letters_hv)
 
@@ -131,12 +143,12 @@ list(
 )
 
 # Outgoing a
-Aneighbours = graph.node_neighbours(letters_hv[letters.index("a")], outgoing=True)
+Aneighbours = graph.node_neighbors(letters_hv[letters.index("a")], outgoing=True)
 
 similarity = functional.cosine_similarity(Aneighbours, letters_hv)
 
 # Incoming b
-Bneighbours = graph.node_neighbours(letters_hv[letters.index("b")], outgoing=False)
+Bneighbours = graph.node_neighbors(letters_hv[letters.index("b")], outgoing=False)
 similarity = functional.cosine_similarity(Bneighbours, letters_hv)
 
 # BINARY TREE
