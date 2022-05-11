c4e2617a61f4f4a51d519339b8ef7f60ba7f0ec3
pereverges <pere.verges@estudiantat.upc.edu>
2022-05-11 02:59:16 +0200

    bloom filter advancing

--- examples/bloom_filters.py
+++ examples/bloom_filters.py
@@ -1,19 +1,13 @@
 # The following two lines are only needed because of this repository organization
-import sys, os
-
+import os
+import sys
+import copy
 sys.path.insert(1, os.path.realpath(os.path.pardir))
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.utils.data as data
-
+from scipy.stats import binom
 # Note: this example requires the torchmetrics library: https://torchmetrics.readthedocs.io
-import torchmetrics
-
+import numpy as np
 from torchhd import functional
-from torchhd import embeddings
-from torchhd import structures
 
 D = 10000
 num_hash_fun = 100
@@ -21,24 +15,65 @@ dens = num_hash_fun/D
 N = 10000
 simul = 10
 
-selection = torch.full((D,N), -1)
+hd = torch.full((D,N), -1)
 
-for i in selection:
+for i in hd:
     perm = torch.randperm(D)[:num_hash_fun]
     i[perm] = 1
 
 thr_limit = torch.tensor([20]*30 + [30]*20 + [40]*15 + [50]*15 + [60]*20)
 
-f_range = torch.range(0,5000,50)
+f_range = torch.range(50,5050,50)
 current_rbf = torch.zeros(num_hash_fun, 3)
 
-for i in f_range:
+for i in range(len(f_range)):
+    F = f_range[i]
     rbf_tpr = torch.zeros(simul)
     rbf_fpr = torch.zeros(simul)
     for j in range(simul):
         rand_perm = torch.randperm(N)
-        ind = rand_perm[:int(i)]
-        ll_ind = rand_perm[int(i):]
-        l_ind = torch.full(N, -1)
-        l_ind[ind] = 1
+        ind = rand_perm[:int(F)]
+        ll_ind = rand_perm[int(F):]
+        # l_ind = torch.full((N,1), 1)
+        rbf = functional.multiset(hd[ind,:])
 
+        positive = torch.tensor(1.0)
+        negative = torch.tensor(-1.0)
+        rbf = torch.where(rbf > -F, positive, negative)
+        rbf_ind = ((rbf != -1).nonzero())
+
+        puncture = int(D*(0.001*(len(rbf_ind)/D)))
+        p_ind = torch.randperm(N)[:puncture]
+
+        rbf[p_ind] = -1
+        dp = functional.bind(hd, rbf)
+        rbf_tpr[j] = (sum(dp[ind,1] != 1)/F.item()).item()
+        rbf_fpr[j] = ((sum(dp[ind,1] != 1)/(N-F.item())).item())
+
+    print(rbf_tpr)
+    print(rbf_fpr)
+
+    current_rbf[int(F),0] = torch.mean(rbf_tpr)
+    current_rbf[int(F),1] = torch.mean(rbf_fpr)
+    current_rbf[int(F),2] = (current_rbf[int(i),0]+(1-current_rbf[int(F),1]))/2
+
+    p1 = dens
+    pdf_bins = binom.pmf(range(0,int(F)+1), int(F), p1)
+    thr = list(range(0,thr_limit[int(i)]))
+    p0_bf = torch.zeros(len(thr))
+
+    for k in thr:
+        p0_bf[k] = sum(pdf_bins[0:thr[k]+1])
+
+    print(pdf_bins)
+    bins2 = list(range(num_hash_fun+1))
+    pdf_bins2 = torch.zeros(len(thr), len(bins2))
+    for k in range(len(thr)):
+        print(k)
+        print(F*num_hash_fun)
+        print(F*num_hash_fun)
+        print((F*num_hash_fun - sum((D*list(range(k)))*pdf_bins[:k])))
+        print(sum((D*list(range(k)))*pdf_bins[:k]))
+        print(F*num_hash_fun)
+
+    break
