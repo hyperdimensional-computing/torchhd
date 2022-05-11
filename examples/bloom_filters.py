68b793453dae1b7403f03061b2984f55adb69fa8
pereverges <pere.verges@estudiantat.upc.edu>
2022-05-11 21:53:08 +0200

    bloom filter first finished version (need to check if the output is correct)

--- examples/bloom_filters.py
+++ examples/bloom_filters.py
@@ -8,13 +8,18 @@ from scipy.stats import binom
 # Note: this example requires the torchmetrics library: https://torchmetrics.readthedocs.io
 import numpy as np
 from torchhd import functional
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 
 D = 10000
 num_hash_fun = 100
 dens = num_hash_fun/D
 N = 10000
 simul = 10
-
+BEST_ABF = []
+CURRENT_BF = []
+BEST_BF = []
 hd = torch.full((D,N), -1)
 
 for i in hd:
@@ -50,12 +55,10 @@ for i in range(len(f_range)):
         rbf_tpr[j] = (sum(dp[ind,1] != 1)/F.item()).item()
         rbf_fpr[j] = ((sum(dp[ind,1] != 1)/(N-F.item())).item())
 
-    print(rbf_tpr)
-    print(rbf_fpr)
-
-    current_rbf[int(F),0] = torch.mean(rbf_tpr)
-    current_rbf[int(F),1] = torch.mean(rbf_fpr)
-    current_rbf[int(F),2] = (current_rbf[int(i),0]+(1-current_rbf[int(F),1]))/2
+    print(i)
+    current_rbf[i,0] = torch.mean(rbf_tpr)
+    current_rbf[i,1] = torch.mean(rbf_fpr)
+    current_rbf[i,2] = (current_rbf[int(i),0]+(1-current_rbf[i,1]))/2
 
     p1 = dens
     pdf_bins = binom.pmf(range(0,int(F)+1), int(F), p1)
@@ -65,15 +68,38 @@ for i in range(len(f_range)):
     for k in thr:
         p0_bf[k] = sum(pdf_bins[0:thr[k]+1])
 
-    print(pdf_bins)
-    bins2 = list(range(num_hash_fun+1))
+    bins2 = range(num_hash_fun+1)
     pdf_bins2 = torch.zeros(len(thr), len(bins2))
+    pdf_bins3 = torch.zeros(len(thr), len(bins2))
     for k in range(len(thr)):
-        print(k)
-        print(F*num_hash_fun)
-        print(F*num_hash_fun)
-        print((F*num_hash_fun - sum((D*list(range(k)))*pdf_bins[:k])))
-        print(sum((D*list(range(k)))*pdf_bins[:k]))
-        print(F*num_hash_fun)
+        l1 = [D*(x) for x in list(range(0,k+1))]
+        exp = (F*num_hash_fun - sum([l1[i]*pdf_bins[:k+1][i] for i in range(len(l1))])) / (F*num_hash_fun)
+        pdf_bins2[k] = torch.tensor(binom.pmf(bins2, num_hash_fun, exp))
+        pdf_bins3[k] = torch.tensor(binom.pmf(bins2, num_hash_fun, (1-p0_bf[k])))
 
-    break
+    tnfp = [ [] for _ in range(len(thr)) ]
+    for j in range(len(thr)):
+        for k in range(num_hash_fun):
+            b1 = sum(pdf_bins2[j][k:])
+            b2 = sum(pdf_bins3[j][k:])
+            tnfp[j].append([b1, b2, (b1+(1-b2))/2])
+
+    THR = torch.zeros(len(thr), 5)
+    for k in range(len(thr)):
+        col0 = [row[0] for row in tnfp[k]]
+        index = [i for i, v in enumerate(col0) if v >= 0.9]
+        val = [row[2] for row in tnfp[k]]
+        ind = len([val[i] for i in index])
+        THR[k][0] = ind-1
+        THR[k][1:4] = torch.tensor(tnfp[k][ind-1])
+
+    THR[:,4] = torch.tensor(thr)
+    ind = torch.max(THR[:,3])
+    BEST_ABF.append(THR[int(ind),:])
+    CURRENT_BF.append(tnfp[0][-1])
+    one_n_opt = round((D/int(F))*np.log(2))
+    if one_n_opt == 0:
+        one_n_opt = 1
+    fpr = pow(1-np.exp(-(one_n_opt*int(F)/D)), one_n_opt)
+    BEST_BF.append([one_n_opt, 1, fpr, (1+(1-fpr))/2])
+    print(BEST_BF)
