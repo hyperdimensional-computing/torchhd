601ec610265d7bdce3a45fe2ae799274f723633a
mikeheddes <mikeheddes@gmail.com>
2022-05-19 15:03:42 -0700

    Add basic function aliasing

--- torchhd/__init__.py
+++ torchhd/__init__.py
@@ -3,9 +3,26 @@ import torchhd.embeddings as embeddings
 import torchhd.structures as structures
 import torchhd.datasets as datasets
 
+from torchhd.functional import (
+    identity_hv,
+    random_hv,
+    level_hv,
+    circular_hv,
+    bind,
+    bundle,
+    permute,
+)
+
 __all__ = [
     "functional",
     "embeddings",
     "structures",
     "datasets",
+    "identity_hv",
+    "random_hv",
+    "level_hv",
+    "circular_hv",
+    "bind",
+    "bundle",
+    "permute",
 ]
