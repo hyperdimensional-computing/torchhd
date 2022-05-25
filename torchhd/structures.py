8a845181dab49e2b769bdd52ec5ecdc6a56d48f6
Mike Heddes <mikeheddes@gmail.com>
2022-05-24 17:27:54 -0700

    Fix graph neighbors (#65)
    * Fix graph neighbors
    
    * Change variable name

--- torchhd/structures.py
+++ torchhd/structures.py
@@ -794,13 +794,10 @@ class Graph:
 
     Args:
         dimensions (int): number of dimensions of the graph.
-        directed (bool): decides if the graph will be directed or not.
+        directed (bool, optional): specify if the graph is directed or not. Default: ``False``.
         dtype (``torch.dtype``, optional): the desired data type of returned tensor. Default: if ``None``, uses a global default (see ``torch.set_default_tensor_type()``).
         device (``torch.device``, optional):  the desired device of returned tensor. Default: if ``None``, uses the current device for the default tensor type (see torch.set_default_tensor_type()). ``device`` will be the CPU for CPU tensor types and the current CUDA device for CUDA tensor types.
-
-    Args:
         input (Tensor): tensor representing a graph hypervector.
-        directed (bool): decides if the graph will be directed or not.
 
     Examples::
 
@@ -817,7 +814,7 @@ class Graph:
         ...
 
     def __init__(self, dim_or_input: int, **kwargs):
-        self.directed = kwargs.get("directed", False)
+        self.is_directed = kwargs.get("directed", False)
         if torch.is_tensor(dim_or_input):
             self.value = dim_or_input
         else:
@@ -861,16 +858,17 @@ class Graph:
             tensor([-1.,  1., -1.,  ...,  1., -1., -1.])
 
         """
-        if self.directed:
-            return functional.bind(node1, node2)
-        else:
+        if self.is_directed:
             return functional.bind(node1, functional.permute(node2))
+        else:
+            return functional.bind(node1, node2)
 
     def node_neighbors(self, input: Tensor, outgoing=True) -> Tensor:
         """Returns the multiset of node neighbors of the input node.
 
         Args:
             input (Tensor): Hypervector representing the node.
+            outgoing (bool, optional): if ``True``, returns the neighboring nodes that ``input`` has an edge to. If ``False``, returns the neighboring nodes that ``input`` has an edge from. This only has effect for directed graphs. Default: ``True``.
 
         Examples::
 
@@ -878,11 +876,13 @@ class Graph:
             tensor([ 1.,  1.,  1.,  ..., -1., -1.,  1.])
 
         """
-        if self.directed:
+        if self.is_directed:
             if outgoing:
-                return functional.permute(functional.bind(self.value, input), shifts=-1)
+                permuted_neighbors = functional.bind(self.value, input)
+                return functional.permute(permuted_neighbors, shifts=-1)
             else:
-                return functional.bind(self.value, functional.permute(input, shifts=1))
+                permuted_node = functional.permute(input, shifts=1)
+                return functional.bind(self.value, permuted_node)
         else:
             return functional.bind(self.value, input)
 
