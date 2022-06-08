23194263bd66f1f7e716ed158122efb518c7fd05
mikeheddes <mikeheddes@gmail.com>
2022-06-08 10:25:20 -0700

    Add tests for hamming distance

--- torchhd/tests/test_similarities.py
+++ torchhd/tests/test_similarities.py
@@ -278,3 +278,132 @@ class TestCosSimilarity:
         similarity = functional.cosine_similarity(hv, hv)
 
         assert similarity.device == device
+
+class TestHammingSimilarity:
+    @pytest.mark.parametrize("dtype", torch_dtypes)
+    def test_shape(self, dtype):
+        if not supported_dtype(dtype):
+            return
+
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+
+        hv = functional.random_hv(2, 100, generator=generator, dtype=dtype)
+        similarity = functional.hamming_similarity(hv[0], hv[1])
+        assert similarity.shape == ()
+
+        hv = functional.random_hv(2, 100, generator=generator, dtype=dtype)
+        similarity = functional.hamming_similarity(hv[0], hv)
+        assert similarity.shape == (2,)
+
+        hv = functional.random_hv(2, 100, generator=generator, dtype=dtype)
+        hv2 = functional.random_hv(4, 100, generator=generator, dtype=dtype)
+        similarity = functional.hamming_similarity(hv, hv2)
+        assert similarity.shape == (2, 4)
+
+        hv1 = functional.random_hv(6, 100, generator=generator, dtype=dtype).view(
+            2, 3, 100
+        )
+        hv2 = functional.random_hv(4, 100, generator=generator, dtype=dtype)
+        similarity = functional.hamming_similarity(hv1, hv2)
+        assert similarity.shape == (2, 3, 4)
+
+    @pytest.mark.parametrize("dtype", torch_dtypes)
+    def test_value(self, dtype):
+        if not supported_dtype(dtype):
+            return
+
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+
+        if dtype == torch.bool:
+            hv = torch.tensor(
+                [
+                    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 1, 0, 0, 1, 0, 1, 1],
+                ],
+                dtype=dtype,
+            )
+
+            res = functional.hamming_similarity(hv, hv)
+            exp = torch.tensor([[10, 7], [7, 10]], dtype=torch.long)
+            assert torch.all(res == exp).item()
+
+        elif dtype in torch_complex_dtypes:
+            hv = torch.tensor(
+                [
+                    [
+                        -0.2510 + 0.9680j,
+                        0.0321 + 0.9995j,
+                        -0.6063 - 0.7953j,
+                        -0.4006 - 0.9162j,
+                        0.4987 - 0.8667j,
+                        -0.3252 - 0.9456j,
+                        -0.2784 + 0.9605j,
+                        -0.8563 + 0.5165j,
+                        0.9061 + 0.4231j,
+                        -0.3801 - 0.9250j,
+                    ],
+                    [
+                        -0.9610 + 0.2766j,
+                        0.9879 - 0.1551j,
+                        -0.4111 - 0.9116j,
+                        -0.8185 + 0.5744j,
+                        -0.8123 + 0.5833j,
+                        0.2966 + 0.9550j,
+                        -0.9958 - 0.0915j,
+                        0.8630 - 0.5052j,
+                        -0.1480 - 0.9890j,
+                        0.5285 - 0.8489j,
+                    ],
+                ],
+                dtype=dtype,
+            )
+
+            res = functional.hamming_similarity(hv, hv)
+            exp = torch.tensor([[10, 0], [0, 10]], dtype=torch.long)
+            assert torch.all(res == exp).item()
+
+        else:
+            hv = torch.tensor(
+                [
+                    [1, 1, -1, 1, -1, 1, -1, 1, -1, 1],
+                    [1, -1, -1, 1, 1, -1, 1, -1, 1, -1],
+                ],
+                dtype=dtype,
+            )
+
+            res = functional.hamming_similarity(hv, hv)
+            exp = torch.tensor([[10, 3], [3, 10]], dtype=torch.long)
+            assert torch.all(res == exp).item()
+
+    @pytest.mark.parametrize("dtype", torch_dtypes)
+    def test_dtype(self, dtype):
+        if not supported_dtype(dtype):
+            return
+
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+
+        hv = functional.random_hv(3, 100, generator=generator, dtype=dtype)
+
+        similarity = functional.hamming_similarity(hv, hv)
+
+        assert similarity.dtype == torch.long
+
+    @pytest.mark.parametrize("dtype", torch_dtypes)
+    def test_device(self, dtype):
+        if not supported_dtype(dtype):
+            return
+
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        hv = functional.random_hv(
+            3, 100, generator=generator, dtype=dtype, device=device
+        )
+
+        similarity = functional.hamming_similarity(hv, hv)
+
+        assert similarity.device == device
