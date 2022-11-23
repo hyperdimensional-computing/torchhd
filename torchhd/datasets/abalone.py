884cbac6791a4f401f648562e4aaba6aebf1a523
Denis Kleyko <denis.kleyko@gmail.com>
2022-11-23 23:56:22 +0100

    Revisting logic of assigning data w.r.t. variables

--- torchhd/datasets/abalone.py
+++ torchhd/datasets/abalone.py
@@ -15,12 +15,15 @@ class Abalone(data.Dataset):
 
     Args:
         root (string): Root directory containing the files of the dataset.
-        train (bool, optional): If True, returns training (sub)set stored in ``train.data`` as further determined by fold_id and fold_train variables.
-            Otherwise tries to return test set if ``test.data`` exists, issues error in case it is not available.
-        fold_id (int, optional): Specifies which fold number to use. Relevant only if train is set to True. The default value of 0 returns the whole data in ``train.data``.
-            Values between 1 and 4 specify, which fold in ``k_fold_cross_val.data`` to use.
-        fold_val (bool, optional): If True, creates dataset using indeces in ``k_fold_cross_val.data`` for for validation part (some odd row) specified by fold_id.
-            Otherwise uses indices the training part (some even row) of the fold. Relevant only if train is set to True and fold_id>0.
+        train (bool, optional): If True, returns training (sub)set from the file storing training data as further determined by fold and hyper_search variables.
+            Otherwise returns a subset of train dataset if hypersearch is performed (hyper_search = True) if not (hyper_search = False) returns test set if it exists.
+            If separate test set is not aviable returns a subset of training dataset as specified in ``conxuntos_kfold.dat`` if fold number is correc. 
+            Otherwise issues an error.
+        fold (int, optional): Specifies which fold number to use. The default value of -1 returns the whole train data from the corresponding file.
+            Values between 0 and 3 specify, which fold in ``conxuntos_kfold.dat`` to use. Relevant only if train is set to True and fold>=0.
+            Indices in even rows (zero indexing) of ``conxuntos_kfold.dat`` correspond to train subsets while indices in odd rows correspond to test subsets.
+        hyper_search (bool, optional): If True, creates dataset using indeces in ``conxuntos.dat``. This split is used for hyperparameter search. The first row corresponds to train indices (used if train = True) 
+            while the second row corresponds to test indices (used if train = False).             
         download (bool, optional): If True, downloads the dataset from the internet and
             puts it in root directory. If dataset is already downloaded, it is not
             downloaded again.
@@ -35,25 +38,31 @@ class Abalone(data.Dataset):
         "1",
         "2",
     ]
+    #Number of folds for cross validation
+    num_folds = 4
+    #Indicates whether the dataset has a separate training file
+    has_test = False
+    #Useful to specify name once to avoid changing it in several places 
+    dataset = "abalone"
 
     def __init__(
         self,
         root: str,
         train: bool = True,
-        fold_id: int = 0,
-        fold_val: bool = False,
+        fold: int = -1,
+        hyper_search: bool = False,
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
         download: bool = False,
     ):
-        root = os.path.join(root, "abalone")
+        root = os.path.join(root, self.dataset)
         root = os.path.expanduser(root)
         self.root = root
         os.makedirs(self.root, exist_ok=True)
 
         self.train = train
-        self.fold_id = fold_id
-        self.fold_val = fold_val
+        self.fold = fold
+        self.hyper_search = hyper_search
         self.transform = transform
         self.target_transform = target_transform
 
@@ -94,9 +103,11 @@ class Abalone(data.Dataset):
             return False
 
         # Check if the root directory contains the required files
-        has_train_file = os.path.isfile(os.path.join(self.root, "abalone_R.dat"))
+        has_train_file = os.path.isfile(os.path.join(self.root, self.dataset+"_R.dat"))
         has_k_fold_file = os.path.isfile(os.path.join(self.root, "conxuntos_kfold.dat"))
-        if has_train_file and has_k_fold_file:
+        has_fold_file = os.path.isfile(os.path.join(self.root, "conxuntos.dat"))
+
+        if has_train_file and has_k_fold_file and has_fold_file:
             return True
 
         # TODO: Add more specific checks like an MD5 checksum
@@ -104,42 +115,114 @@ class Abalone(data.Dataset):
         return False
 
     def _load_data(self):
-        train_data_file = "abalone_R.dat"
-        val_file = "conxuntos_kfold.dat"
+        # Files with pregenerated folds with indices
+        k_fold_file = "conxuntos_kfold.dat"
+        hyper_search_file = "conxuntos.dat"
+        
+        # Names of data files depending on whether test data comes separately
+        if self.has_test: 
+            train_data_file = self.dataset + "_train_R.dat"
+            test_data_file = self.dataset + "_test_R.dat"
+            # Load test data
+            test_data = pd.read_csv(os.path.join(self.root, test_data_file), sep="\t", header=None, skiprows=1,)
+            # Skip number & target columns 
+            test_targets = test_data.values[:, -1].astype(int)
+            test_data = test_data.values[:, 1:-1]
+            
+        else:
+            train_data_file = self.dataset + "_R.dat"       
 
+        # Load train data
+        train_data = pd.read_csv(os.path.join(self.root, train_data_file), sep="\t", header=None, skiprows=1,)
+        # Skip number & target columns 
+        train_targets_all = train_data.values[:, -1].astype(int)
+        train_data_all = train_data.values[:, 1:-1]
+        
+        # Load fold used in hyperparameter search if necessary
+        if self.hyper_search:
+                hyper_search_ind = pd.read_csv(os.path.join(self.root, hyper_search_file), sep=" ", header=None).values
+        # Load k-fold if necessary
+        if self.fold>=0:
+                cross_val_ind = pd.read_csv(os.path.join(self.root, k_fold_file), sep=" ", header=None).values
+        
+        #train = True  - some form of train data is requested
         if self.train:
-            train_data = pd.read_csv(
-                os.path.join(self.root, train_data_file),
-                sep="\t",
-                header=None,
-                skiprows=1,
-            )
-            train_data_all = train_data.values[:, 1:-1]
-            train_targets_all = train_data.values[:, -1].astype(int)
-
-            if self.fold_id == 0:
-                self.data = torch.tensor(train_data_all, dtype=torch.float)
-                self.targets = torch.tensor(train_targets_all, dtype=torch.long)
-            else:
-                cross_val_ind = pd.read_csv(
-                    os.path.join(self.root, val_file), sep=" ", header=None
-                )
-                cross_val_ind = cross_val_ind.values[:, 0:-1]
-                fold_index = (self.fold_id - 1) * 2 + int(self.fold_val)
-                k_fold_indices = cross_val_ind[
-                    fold_index, np.invert(np.isnan(cross_val_ind[fold_index, :]))
+            # If hyperparameter search is being performed
+            if self.hyper_search: 
+                indices = hyper_search_ind[
+                    0, np.invert(np.isnan(hyper_search_ind[0, :]))
                 ].astype(int)
                 self.data = torch.tensor(
-                    train_data_all[k_fold_indices, :], dtype=torch.float
+                    train_data_all[indices, :], dtype=torch.float
                 )
                 self.targets = torch.tensor(
-                    train_targets_all[k_fold_indices], dtype=torch.long
-                )
-
+                    train_targets_all[indices], dtype=torch.long
+                )   
+            # If regular training is going on    
+            else:  
+                # If not fold is specified
+                if self.fold == -1:
+                    self.data = torch.tensor(train_data_all, dtype=torch.float)
+                    self.targets = torch.tensor(train_targets_all, dtype=torch.long)
+                elif (self.fold>=0) and (self.fold<self.num_folds):
+                    #Identify which row of cross_val_ind to index - even number
+                    fold_index = self.fold  * 2 
+                    indices = cross_val_ind[
+                        fold_index, np.invert(np.isnan(cross_val_ind[fold_index, :]))
+                    ].astype(int)
+                    self.data = torch.tensor(
+                        train_data_all[indices, :], dtype=torch.float
+                    )
+                    self.targets = torch.tensor(
+                        train_targets_all[indices], dtype=torch.long
+                    )                   
+                #Wrong fold number
+                else:
+                    raise ValueError(
+                        f"No such fold number. Please check that it is specified correctly"
+                    )                       
+        #train = False  - some form of test data is requested
         else:
-            raise ValueError(
-                f"This dataset does not have a separate file for test data."
-            )
+            # If hyperparameter search is being performed
+            if self.hyper_search: 
+                indices = hyper_search_ind[
+                    1, np.invert(np.isnan(hyper_search_ind[1, :]))
+                ].astype(int)
+                self.data = torch.tensor(
+                    train_data_all[indices, :], dtype=torch.float
+                )
+                self.targets = torch.tensor(
+                    train_targets_all[indices], dtype=torch.long
+                )   
+            # If regular testing is going on    
+            else:   
+                #If there is a dedicted file 
+                if self.has_test:
+                    self.data = torch.tensor(
+                        test_data, dtype=torch.float
+                    )
+                    self.targets = torch.tensor(
+                        test_targets, dtype=torch.long
+                    )   
+                #In case there is no dedicated use crossfold
+                else:
+                    #Check that fold number is within limits otherwise issue error
+                    if (self.fold>=0) and (self.fold<self.num_folds):
+                        #Identify which row of cross_val_ind to index - odd number
+                        fold_index = self.fold  * 2 + 1
+                        indices = cross_val_ind[
+                            fold_index, np.invert(np.isnan(cross_val_ind[fold_index, :]))
+                        ].astype(int)
+                        self.data = torch.tensor(
+                            train_data_all[indices, :], dtype=torch.float
+                        )
+                        self.targets = torch.tensor(
+                            train_targets_all[indices], dtype=torch.long
+                        )                        
+                    else:                        
+                        raise ValueError(
+                            f"This dataset does not have a separate file for test data. Please check that fold is specified correctly"
+                        )
 
     def download(self):
         """Download the data if it doesn't exist already."""
@@ -164,5 +247,6 @@ class Abalone(data.Dataset):
         # Extract archive
         with tarfile.open(archive_path) as file:
             for member in file.getmembers():
-                if member.name.startswith("abalone"):
+                if member.name.startswith(self.dataset):
                     file.extract(member, data_dir)
+
