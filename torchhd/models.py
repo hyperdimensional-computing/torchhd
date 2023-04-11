0bd2bdf81b8695faff17c6b5d51a4fff9036028a
pereverges <pere.verges@bsc.es>
2023-04-11 08:32:43 -0700

    HDCArena, fix

--- torchhd/models.py
+++ torchhd/models.py
@@ -90,6 +90,11 @@ class Centroid(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
 
+        self.similarity_sum = 0
+        self.count = 0
+        self.error_similarity_sum = 0
+        self.error_count = 0
+
         weight = torch.empty((out_features, in_features), **factory_kwargs)
         self.weight = Parameter(weight, requires_grad=requires_grad)
         self.reset_parameters()
@@ -107,6 +112,27 @@ class Centroid(nn.Module):
     def add(self, input: Tensor, target: Tensor, lr: float = 1.0) -> None:
         """Adds the input vectors scaled by the lr to the target prototype vectors."""
         self.weight.index_add_(0, target, input, alpha=lr)
+
+    @torch.no_grad()
+    def add_adapt(self, input: Tensor, target: Tensor, lr: float = 1.0) -> None:
+        """Adds the input vectors scaled by the lr to the target prototype vectors."""
+        logit = self(input)
+        pred = logit.argmax(1)
+        is_wrong = target != pred
+
+        # cancel update if all predictions were correct
+        if is_wrong.sum().item() == 0:
+            return
+
+        self.weight.index_add_(0, target, input, alpha=lr)
+
+        input = input[is_wrong]
+        target = target[is_wrong]
+        pred = pred[is_wrong]
+
+        self.weight.index_add_(0, target, input)
+        self.weight.index_add_(0, pred, -input)
+
 
     @torch.no_grad()
     def add_online(self, input: Tensor, target: Tensor, lr: float = 1.0) -> None:
@@ -135,10 +161,52 @@ class Centroid(nn.Module):
         pred = pred[is_wrong]
 
         alpha1 = 1.0 - logit.gather(1, target.unsqueeze(1))
-        alpha2 = logit.gather(1, pred.unsqueeze(1)) - 1.0
+        alpha2 = 1.0 - logit.gather(1, pred.unsqueeze(1))
 
         self.weight.index_add_(0, target, lr * alpha1 * input)
+        self.weight.index_add_(0, pred, lr * alpha2 * -input)
+
+
+    @torch.no_grad()
+    def add_adjust(self, input: Tensor, target: Tensor, lr: float = 1.0) -> None:
+        r"""Only updates the prototype vectors on wrongly predicted inputs.
+
+        Implements the iterative training method as described in `OnlineHD: Robust, Efficient, and Single-Pass Online Learning Using Hyperdimensional System <https://ieeexplore.ieee.org/abstract/document/9474107>`_.
+
+        Adds the input to the mispredicted class prototype scaled by :math:`\epsilon - 1`
+        and adds the input to the target prototype scaled by :math:`1 - \delta`,
+        where :math:`\epsilon` is the cosine similarity of the input with the mispredicted class prototype
+        and :math:`\delta` is the cosine similarity of the input with the target class prototype.
+        """
+        # Adapted from: https://gitlab.com/biaslab/onlinehd/-/blob/master/onlinehd/onlinehd.py
+        logit = self(input)
+        pred = logit.argmax(1)
+        is_wrong = target != pred
+
+        self.similarity_sum += logit.max(1).values.item()
+        self.count += 1
+        if self.error_count == 0:
+            val = self.similarity_sum / self.count
+        else:
+            val = self.error_similarity_sum / self.error_count
+        if is_wrong.sum().item() == 0:
+            if logit.max(1).values.item() < val:
+                self.weight.index_add_(0, target, input)
+            return
+
+        self.error_count += 1
+        self.error_similarity_sum += logit.max(1).values.item()
+
+        logit = logit[is_wrong]
+        input = input[is_wrong]
+        target = target[is_wrong]
+        pred = pred[is_wrong]
+
+        alpha1 = 1.0 - logit.gather(1, target.unsqueeze(1))
+        self.weight.index_add_(0, target, lr * alpha1 * input)
+        alpha2 = logit.gather(1, pred.unsqueeze(1)) - 1
         self.weight.index_add_(0, pred, lr * alpha2 * input)
+
 
     @torch.no_grad()
     def normalize(self, eps=1e-12) -> None:
