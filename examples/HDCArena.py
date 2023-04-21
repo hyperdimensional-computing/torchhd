587d376e63ad6db0189be08ef46058069fd1c730
pereverges <pere.verges@bsc.es>
2023-04-20 17:17:41 -0700

    HDCArena, model

--- examples/HDCArena.py
+++ examples/HDCArena.py
@@ -337,7 +337,7 @@ DIMENSIONS = [10000]
 ENCODINGS = ["hashmap", "flocet", "density", "random", "sinusoid"]
 #ENCODINGS = ["sinusoid"]
 #METHODS = ["add"]
-METHODS = ["add","add_adapt","add_online","add_adjust","add_adjust_2","add_adjust_3"]
+METHODS = ["add_adjust_4"]
 #METHODS = ["neural"]
 RETRAIN = [True,False]
 
--- torchhd/models.py
+++ torchhd/models.py
@@ -266,6 +266,49 @@ class Centroid(nn.Module):
         self.weight.index_add_(0, target, input)
         self.weight.index_add_(0, pred, -input)
 
+
+    @torch.no_grad()
+    def add_adjust_4(self, input: Tensor, target: Tensor, lr: float = 1.0) -> None:
+        r"""Only updates the prototype vectors on wrongly predicted inputs.
+
+        Implements the iterative training method as described in `OnlineHD: Robust, Efficient, and Single-Pass Online Learning Using Hyperdimensional System <https://ieeexplore.ieee.org/abstract/document/9474107>`_.
+
+        Adds the input to the mispredicted class prototype scaled by :math:`\epsilon - 1`
+        and adds the input to the target prototype scaled by :math:`1 - \delta`,
+        where :math:`\epsilon` is the cosine similarity of the input with the mispredicted class prototype
+        and :math:`\delta` is the cosine similarity of the input with the target class prototype.
+        """
+        # Adapted from: https://gitlab.com/biaslab/onlinehd/-/blob/master/onlinehd/onlinehd.py
+        logit = self(input)
+        pred = logit.argmax(1)
+        is_wrong = target != pred
+
+        self.similarity_sum += logit.max(1).values.item()
+        self.count += 1
+        if self.error_count == 0:
+            val = self.similarity_sum / self.count
+        else:
+            val = self.error_similarity_sum / self.error_count
+        if is_wrong.sum().item() == 0:
+            if logit.max(1).values.item() < val:
+                self.weight.index_add_(0, target, input)
+            return
+
+        self.error_count += 1
+        self.error_similarity_sum += logit.max(1).values.item()
+
+        logit = logit[is_wrong]
+        input = input[is_wrong]
+        target = target[is_wrong]
+        pred = pred[is_wrong]
+
+        alpha1 = 1.0 - logit.gather(1, target.unsqueeze(1))
+        self.weight.index_add_(0, target, lr * alpha1 * input)
+        self.weight.index_add_(0, target, lr * alpha1 * input)
+        alpha2 = logit.gather(1, pred.unsqueeze(1)) - 1
+        self.weight.index_add_(0, pred, lr * alpha2 * input)
+
+
     @torch.no_grad()
     def normalize(self, eps=1e-12) -> None:
         """Transforms all the class prototype vectors into unit vectors.
