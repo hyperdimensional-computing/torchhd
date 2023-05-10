161c1a8806620b82da919c7b4fc8282a35fca078
pereverges <pere.verges@bsc.es>
2023-05-10 11:46:43 -0700

    New arena

--- HDCArena/SinglePass/vanillaHD.py
+++ HDCArena/SinglePass/vanillaHD.py
@@ -13,6 +13,7 @@ def train_vanillaHD(train_loader, device, encode, model):
 
 
 def test_vanillaHD(test_loader, device, encode, model, accuracy):
+    model.normalize()
     with torch.no_grad():
         for samples, labels in tqdm(test_loader, desc="Testing"):
             samples = samples.to(device)
