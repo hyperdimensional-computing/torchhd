9510fbc7ac3756b719adc312211e29e5c31b66d5
pereverges <pere.verges@bsc.es>
2023-05-12 14:14:54 -0700

    New arena

---
+++ HDCArena/SinglePass/adjustHD.py
@@ -0,0 +1,23 @@
+import torch
+from tqdm import tqdm
+
+
+def train_adjustHD(train_loader, device, encode, model):
+    with torch.no_grad():
+        for samples, labels in tqdm(train_loader, desc="Training"):
+            samples = samples.to(device)
+            labels = labels.to(device)
+
+            samples_hv = encode(samples)
+            model.add_adjust(samples_hv, labels)
+
+
+def test_adjustHD(test_loader, device, encode, model, accuracy):
+    model.normalize()
+    with torch.no_grad():
+        for samples, labels in tqdm(test_loader, desc="Testing"):
+            samples = samples.to(device)
+
+            samples_hv = encode(samples)
+            outputs = model(samples_hv, dot=True)
+            accuracy.update(outputs.to(device), labels.to(device))
