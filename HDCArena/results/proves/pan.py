89e70b15b7f695df398b550b05563e576923bc80
pereverges <pere.verges@bsc.es>
2023-05-24 15:33:14 -0700

    New arena

--- HDCArena/results/proves/pan.py
+++ HDCArena/results/proves/pan.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv(
-    "/Users/verges/Documents/PhD/TorchHd/torchhd/HDCArena/results/proves/rvfl"
+    "/Users/verges/Documents/PhD/TorchHd/torchhd/HDCArena/results/results1684767254.650597.csv"
 )
 print(df.head())
+df = df[df['partial_data'] == 0.25]
+df = df[df['robustness_failed_dimensions'] == 0]
+df = df[df['method'] == 'adjust']
 
 mean_accuracy_by_dimension_and_method = df.groupby(["name", "method"])[
     "accuracy"
@@ -14,4 +17,3 @@ variance_accuracy_by_dimension_and_method = df.groupby(["name", "method"])[
 ].var()
 
 print(mean_accuracy_by_dimension_and_method.mean())
-print(variance_accuracy_by_dimension_and_method.mean())
