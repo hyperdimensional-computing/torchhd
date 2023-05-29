a461031b1d088d2c951522b4a0b1d2babecf2027
pereverges <pere.verges@bsc.es>
2023-05-29 10:07:39 -0700

    New arenaa

--- HDCArena/methods_selection.py
+++ HDCArena/methods_selection.py
@@ -816,13 +816,13 @@ configs = [
 
 configs = [
     {
-        "method": "quant_iterative",
+        "method": "sparse_iterative",
         "multi_reduce_subclass": None,
         "threshold": None,
-        "lr": 1.5,
+        "lr": 1,
         "epsilon": 0.01,
-        "model_quantize": "binary",
-        "model_sparse": None,
+        "model_quantize": None,
+        "model_sparse": "class",
         "sparsity": None,
         "lazy_regeneration": None,
         "model_neural": None,
@@ -831,7 +831,7 @@ configs = [
         "beta": None,
         "theta": None,
         "chunks": None,
-        "s": None,
+        "s": 0.5,
     },
 ]
 
