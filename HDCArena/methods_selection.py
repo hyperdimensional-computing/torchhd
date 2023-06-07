a21c3eb96289b99c8f934d5acff6ffe21f64993c
pereverges <pere.verges@bsc.es>
2023-06-06 21:09:51 -0700

    New arenaa

--- HDCArena/methods_selection.py
+++ HDCArena/methods_selection.py
@@ -1044,7 +1044,7 @@ configs = [
 
 configs = [
     {
-        "method": "adjust",
+        "method": "online",
         "multi_reduce_subclass": None,
         "threshold": None,
         "lr": 1,
